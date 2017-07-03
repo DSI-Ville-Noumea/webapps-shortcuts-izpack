cecfcf390ab83b5b8ca42a22cf082b7ffc5ed6a5
mbarre <michele.barre@ville-noumea.nc>
2017-07-03 16:46:35 +1100

    pack DPM, fic #8

---
+++ src/main/izpack/dpm/dpmmc/launch_dpmmc.bat
@@ -0,0 +1,3 @@
+CD C:\Program Files\Mozilla Firefox\
+start firefox.exe http://dpmmc.site-mairie.noumea.nc
+exit;
---
+++ src/main/izpack/dpm/geoloc/launch_geoloc.bat
@@ -0,0 +1,3 @@
+CD C:\Program Files\Mozilla Firefox\
+start firefox.exe http://dpm.geoloc.site-mairie.noumea.nc/
+exit;
