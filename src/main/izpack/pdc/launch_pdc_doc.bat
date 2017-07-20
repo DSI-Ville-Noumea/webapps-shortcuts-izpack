eb7067f00af9994688b7b2e36257bb6c594c4f82
mbarre <michele.barre@ville-noumea.nc>
2017-07-20 15:36:08 +1100

    fix owncloud url

--- src/main/izpack/pdc/launch_pdc_doc.bat
+++ src/main/izpack/pdc/launch_pdc_doc.bat
@@ -1,3 +1,3 @@
 CD C:\Program Files\Mozilla Firefox\
-start firefox.exe https://owncloud.ville-noumea.nc/public.php?service=files&t=b9f816e9f1eddaf4f0aef008f8462963
+start firefox.exe "https://owncloud.ville-noumea.nc/public.php?service=files&t=b9f816e9f1eddaf4f0aef008f8462963"
 exit;
